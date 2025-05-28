395b0417c9006cca15ff4ab1144307146230414b
Shubham Barman <barmanshubham1822@gmail.com>
2025-05-28 08:36:32 +0530

    add 8 files for 100 +python code

---
+++ 100_pythoncode/1_add_input.py
@@ -0,0 +1,8 @@
+a = int(input("enter your first number: "))
+b = int(input("enter your second number: "))
+print(a + b)
+
+
+a = float(input("enter your first number: "))
+b = float(input("enter your second number: "))
+print(a + b)
---
+++ 100_pythoncode/3_square_root.py
@@ -0,0 +1,7 @@
+num = int(input("Enter a number to find its square root: "))
+sr = num**0.5
+print(f"the Squareroot of number : {num} is {sr}")
+
+num_2 = int(input("Enter a number to find its square root: "))
+sr_1 = num_2**1 / 2
+print(f"the Squareroot of number : {num_2} is {sr_1}")
