7f39b130d04834576106098e0a3b0a44842c21e8
Shubham Barman <barmanshubham1822@gmail.com>
2025-06-04 19:12:17 +0530

    save all

--- 100_pythoncode/3_square_root.py
+++ 100_pythoncode/3_square_root.py
@@ -3,5 +3,5 @@ sr = num**0.5
 print(f"the Squareroot of number : {num} is {sr}")
 
 num_2 = int(input("Enter a number to find its square root: "))
-sr_1 = num_2**1 / 2
+sr_1 = num_2 ** (1 / 2)
 print(f"the Squareroot of number : {num_2} is {sr_1}")
